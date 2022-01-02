4aa5ef8476fc7dbbb83c82f4c07bb6be633b8cf9
i.s.pyavkin <i.s.pyavkin@tinkoff.ru>
2022-01-02 13:44:48 +0300

    SI-2 upgrades: fixed messages en -> rus

--- main.py
+++ main.py
@@ -31,15 +31,24 @@ def save_entry(first_name, last_name, username, chat_id, stock):
 
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Oh hohoho! Hello there, secret trader. Please, send me name of stock you want your reciver to buy (e.g. APPL 1000$ x 2 = 2000$)")
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Охохо! Привет, секретный инвестор! 📈\n" +
+             "Пожалуйста, отправь мне в обратном сообщении акцию, которую ты хочешь, чтобы купил твой получатель\n" +
+             "(соблюдай формат, следуя примеру: `Apple (AAPL) 133,37$ x 1 = 9887Р`)\n" +
+             "И не забудь про интервал 500..1000Р за сумму сделки"
+    )
 
 
 def update_paper(update, context):
     save_entry(first_name=update.effective_chat.first_name, last_name=update.effective_chat.last_name,
                username=update.effective_chat.username, chat_id=update.effective_chat.id, stock=update.message.text)
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Oh hohoho! Good choice you have there, I will save it. You can change it by sending me name of the stock again")
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Охохо! Отличный выбор! Я его сохраню.\n" +
+             "Ты можешь изменить его, просто отправив мне новый вариант в сообщении.\n" +
+             "Совсем скоро мы начнем жеребьевку и ты узнаешь, что выпало тебе! 💸"
+    )
 
 
 def rofl(update, context):
